09367fd734f5ca2aaa75e5a324fffa8528c3a4fe
Logan Fossenier <logan.fossenier@gmail.com>
2024-01-23 14:18:58 -0600

    chore: refactor code

--- love_o_meter/love_o_meter.ino
+++ love_o_meter/love_o_meter.ino
@@ -8,10 +8,48 @@ void setup()
 {
     Serial.begin(9600);
     baselineTemp = adc_to_celsius(sensorPin);
+
+    for (int pinNumber = 2; pinNumber < 5; pinNumber++)
+    {
+        pinMode(pinNumber, OUTPUT);
+        digitalWrite(pinNumber, LOW);
+    }
 }
 
 void loop()
 {
+    int activeLed = 0;
+
+    // read the temperature
+    int temperature = adc_to_celsius(sensorPin);
+
+    // determine active LED (2 < +1C, 3 > +1C, 4 > +2C)
+    if (temperature > baselineTemp + 2)
+    {
+        activeLed = 4;
+    }
+    else if (temperature > baselineTemp + 1)
+    {
+        activeLed = 3;
+    }
+    else
+    {
+        activeLed = 2;
+    }
+
+    // illuminate correct LED
+    for (int pinNumber = 2; pinNumber < 5; pinNumber++)
+    {
+        if (pinNumber == activeLed)
+        {
+            digitalWrite(pinNumber, HIGH);
+        }
+        else
+        {
+            digitalWrite(pinNumber, LOW);
+        }
+    }
+    delay(5);
 }
 
 int adc_to_celsius(int sensorPin)

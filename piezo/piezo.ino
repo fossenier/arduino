10ded8954a84bf4c5a12bfe2ad742ea9fa395a2e
Logan Fossenier <logan.fossenier@gmail.com>
2024-01-29 14:54:21 -0600

    feat: initialize piezo code

---
+++ piezo/piezo.ino
@@ -0,0 +1,22 @@
+/*
+Takes in values from a phototransistor and plays a frequency to a piezo based on the light range.
+*/
+
+// delay tracking
+unsigned long previousMillis = 0;
+const long interval = 250;
+
+// input output
+// const int piezoPin =
+// const int photoPin =
+
+int sensorLow = 1023;
+int sensorHigh = 0;
+
+void setup()
+{
+}
+
+void loop()
+{
+}

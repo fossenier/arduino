8ff867f2d5166076153a202c2d7a1eb001a13f29
Logan Fossenier <logan.fossenier@gmail.com>
2024-01-29 15:07:08 -0600

    feat: add frequency allocation, piezo control, and timing

--- piezo/piezo.ino
+++ piezo/piezo.ino
@@ -4,19 +4,59 @@ Takes in values from a phototransistor and plays a frequency to a piezo based on
 
 // delay tracking
 unsigned long previousMillis = 0;
-const long interval = 250;
+const unsigned long interval = 250;
 
 // input output
-// const int piezoPin =
-// const int photoPin =
+const int photoPin = A0;
+const int piezoPin = 8;
+const int ledPin = 13;
 
 int sensorLow = 1023;
 int sensorHigh = 0;
 
 void setup()
 {
+    // turn on LED to signal start of calibration
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, HIGH);
+
+    while (millis() < 5000)
+    {
+        // calibrate sensor to the ambient lighting
+        int sensorValue = analogRead(photoPin);
+        if (sensorValue > sensorHigh)
+        {
+            sensorHigh = sensorValue;
+        }
+        if (sensorValue < sensorLow)
+        {
+            sensorLow = sensorValue;
+        }
+    }
+
+    // turn off LED to signal end of calibration
+    digitalWrite(ledPin, LOW);
 }
 
 void loop()
 {
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis >= interval)
+    {
+        // play the frequency to the piezo
+        previousMillis = currentMillis;
+        int frequency = piezoFrequency();
+        tone(piezoPin, frequency, 10);
+    }
+}
+
+int piezoFrequency()
+{
+    // read the phototransistor
+    int sensorValue = analogRead(photoPin);
+
+    // map the sensor value to a frequency
+    int frequency = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
+    return frequency;
 }

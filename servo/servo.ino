5ee013d71ca8837006fd70e9760b0cec051104ea
Logan Fossenier <logan.fossenier@gmail.com>
2024-02-02 19:49:12 -0600

    feat: add servo library and value mapping

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,16 +1,32 @@
 /*
  */
+#include <Servo.h>
+
+Servo myServo;
 
 int potentiometer_pin = A0;
+int servo_pin = 9;
+int potentiometer_value;
+int servo_angle;
 
 void setup()
 {
+    myServo.attach(servo_pin);
+
     Serial.begin(9600);
 }
 
 void loop()
 {
-    int potentiometer_value = analogRead(potentiometer_pin);
-    Serial.println(potentiometer_value);
-    delay(100);
+    potentiometer_value = analogRead(potentiometer_pin);
+    Serial.print("Potentiometer value: ");
+    Serial.print(potentiometer_value);
+
+    servo_angle = map(potentiometer_value, 0, 1023, 0, 180);
+    Serial.print("Servo angle: ");
+    Serial.println(servo_angle);
+
+    myServo.write(servo_angle);
+
+    delay(15);
 }

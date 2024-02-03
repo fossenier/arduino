3faefe95fbb0e62485e41dc2e297041b92013090
Logan Fossenier <logan.fossenier@gmail.com>
2024-02-02 19:33:44 -0600

    feat: servo test with servo on serial monitor

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,9 +1,16 @@
 /*
  */
+
+int potentiometer_pin = A0;
+
 void setup()
 {
+    Serial.begin(9600);
 }
 
 void loop()
 {
+    int potentiometer_value = analogRead(potentiometer_pin);
+    Serial.println(potentiometer_value);
+    delay(100);
 }
